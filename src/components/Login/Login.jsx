1aeac86a4c9b5ce995afd1268828df44141b8fbf
Demid Zainulin <parbiska@gmail.com>
2022-09-28 16:01:49 +0800

    some changes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,8 +4,26 @@ const Login = props => {
     return (
         <div className={styles.login}>
             <h1 className={styles.title}>Login</h1>
+            <LoginForm></LoginForm>
         </div>
     )
 };
+
+const LoginForm = props => (
+    <form className={styles.form}>
+        <div className={styles.login}>
+            <input className={styles.input} placeholder='Login' />
+        </div>
+        <div className={styles.password}>
+            <input className={styles.input} placeholder='Password' />
+        </div>
+        <div className={styles.remember}>
+            <input type='checkbox' /> Remember me
+        </div>
+        <div className={styles.button}>
+            <button className={styles.btn}>Login</button>
+        </div>
+    </form>
+)
 
 export default Login;
